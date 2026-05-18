945a00c021fb8d9ef87ca1e2b579defe4482c8ba
VELALAGAN Arushan <12504806@s30404.iutv.univ-paris13.fr>
2026-05-18 16:42:52 +0200

    AJOUT POUR JOHN

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -5,4 +5,15 @@ public class HelloWorld {
 		System.out.println("Hello World !");
 	}
 
+	public class HelloWorld {
+ 
+	public static void main(String[] args) {
+		System.out.println("Ciao mondo !");
+		System.out.println("Hello World !");
+	}
+ 
+}
+
+
+
 }
